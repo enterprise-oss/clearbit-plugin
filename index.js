9e405c47192f8eb0b44e9ee87042222748b319c7
Sam Bauch <sbauch@gmail.com>
2021-01-14 16:36:01 -0500

    node imports

--- index.js
+++ index.js
@@ -1,4 +1,4 @@
-import { Client } from 'clearbit';
+var Client = require('clearbit').Client;
 
 async function setupPlugin({ config, global }) {
     global.clearbitKey = config.clearbitKey;
@@ -8,9 +8,12 @@ async function setupPlugin({ config, global }) {
 async function processEvent(event, { config }) {
     const Reveal = new Client({key: global.clearbitKey}).Reveal;
 
-    if (event.properties) {
+    if (event.properties['$ip']) {
         Reveal.find(event.properties['$ip']).then(function (company) {
             console.log(company);
+            event.properties['cb-raw'] = company;
+
+            return event
         })
     }
 
