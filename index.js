4fe5ca7e8a67a7c52f2432badad76a397c34ecf2
Sam Bauch <sbauch@gmail.com>
2021-01-14 18:01:08 -0500

    buffer

--- index.js
+++ index.js
@@ -7,18 +7,16 @@ async function setupPlugin({ config, global }) {
 
 async function processEvent(event, { global }) {
   if (event.properties['$ip']) {
-      const response = await fetch('https://reveal.clearbit.com/v1/companies/find?ip=' + event.properties['$ip'], {
+      fetch('https://reveal.clearbit.com/v1/companies/find?ip=' + event.properties['$ip'], {
           headers: {
             Authorization: global.clearbitAuth
           }
       })
       .then((resp) => resp.json())
       .then((data) => {
-        event.properties['companyName'] = response.company.name;
-        event.properties['companyDomain'] = response.company.domain;
-      
-        console.log(data)
-        return event
+        event.properties['companyName'] = data.company.name;
+        event.properties['companyDomain'] = data.company.domain;
+              return event
       })
   } else {
       return event
